650303a7daa3faa8148f13cd80d7682fbf0b98b5
vignesh22396 <rvignesh543@gmail.com>
2026-07-24 20:13:46 +0530

    24 night changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -43,3 +43,39 @@ export const registerUser = async (req, res) => {
     }
 
 };
+
+export const loginUser = async(req, res) => {
+ 
+    try{
+        const { email, password } = req.body;
+    
+        const user = await User.findOne({email});
+        
+        if (!user){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Login successful"
+        })
+    }catch(error){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+
+};
