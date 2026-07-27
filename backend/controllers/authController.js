0d7b0f16f0eaec6f99de0d6bffdb51df1d5ee93d
vignesh22396 <rvignesh543@gmail.com>
2026-07-27 20:06:02 +0530

    27 day changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
 
@@ -7,9 +8,13 @@ export const registerUser = async (req, res) => {
 
         const { name, email, password } = req.body;
 
+        console.log("===== REGISTER API CALLED =====");
+        console.log("Email:", email);
+
         // Check if user already exists
         const existingUser = await User.findOne({ email });
 
+        console.log("Existing User:", existingUser);
         
         if (existingUser) {
             return res.status(400).json({
@@ -35,6 +40,8 @@ export const registerUser = async (req, res) => {
 
     } catch (error) {
 
+        console.log(error);
+
         res.status(500).json({
             success: false,
             message: error.message
@@ -48,8 +55,13 @@ export const loginUser = async(req, res) => {
  
     try{
         const { email, password } = req.body;
+
+        console.log("Email from request:", email);
+        console.log("Password from request:", password);
     
         const user = await User.findOne({email});
+
+        console.log("User from DB:", user);
         
         if (!user){
             return res.status(400).json({
@@ -60,6 +72,8 @@ export const loginUser = async(req, res) => {
 
         const isMatch = await bcrypt.compare(password, user.password);
 
+        console.log("Password Match:", isMatch);
+
         if (!isMatch){
             return res.status(400).json({
                 success: false,
@@ -67,10 +81,29 @@ export const loginUser = async(req, res) => {
             })
         }
 
+        const token = jwt.sign(
+            {
+                id: user._id,
+                email: user.email,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
         res.status(200).json({
             success: true,
-            message: "Login successful"
-        })
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
     }catch(error){
         res.status(500).json({
             success: false,
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,11 @@ import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
-
-router.post("/register", registerUser);
+ 
+router.post("/register", (req, res, next) => {
+    console.log("REGISTER ROUTE HIT");
+    next();
+}, registerUser);
 
 router.post("/login", loginUser);
 
