82f433d0ce7236178a4fd1a021fd9162338f8a7b
rvignesh543@gmail.com <rvignesh543@gmail.com>
2026-08-11 08:48:19 +0530

    Configure production CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://your-vercel-url.vercel.app",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
